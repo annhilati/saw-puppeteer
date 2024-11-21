4e6d9bb2fa012a9dd8ddff8a2e451e0f8e49a3b7
Annhilati <antbe.artis@web.de>
2024-11-21 17:30:55 +0100

    Update kurse-anton.js

--- kurse-anton.js
+++ kurse-anton.js
@@ -1,4 +1,11 @@
-const kursIDs = [8372, 5544, 8532, 8528, 8609];
+const kursIDs = [
+    5544,
+    7642,
+    8372,
+    8528,
+    8532,
+    8562,
+    8609];
 
 kursIDs.forEach(kursID => {
     const kursData = JSON.stringify({ kursID });
