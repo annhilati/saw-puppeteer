14880137605a884f159b0618722e101b2c9db908
Joshi <126943242+Joshi1531@users.noreply.github.com>
2024-11-23 11:56:58 +0100

    Update kurse-josua.js

--- kurse-josua.js
+++ kurse-josua.js
@@ -1,10 +1,11 @@
-const kursIDs = [7788, 
+const kursIDs = [7786,
+                 7788, 
                  8176, 
                  5544, 
                  8606, 
                  8699, 
                  8634, 
-                 8681, 
+                 8530, 
                  6286];
 
 kursIDs.forEach(kursID => {
