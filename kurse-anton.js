4da7a6914b862eb1d63e33aa2881bf1b1ab638d8
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-11-25 14:18:16 +0100

    Aktualisieren von kurse-anton.js

--- kurse-anton.js
+++ kurse-anton.js
@@ -1,4 +1,4 @@
-const kursIDs = [8372, 7835, 8615, 5544, 8532, 8528, 5493, 8620];
+const kursIDs = [8372, 8621, 8615, 5544, 8532, 8528, 5493, 8620];
 
 kursIDs.forEach(kursID => {
     const kursData = JSON.stringify({ kursID });
