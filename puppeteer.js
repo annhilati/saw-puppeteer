c0951b278686a62f90e6f6c9a36de5d6e78fb968
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-12-21 17:57:12 +0100

    Update puppeteer.js

--- puppeteer.js
+++ puppeteer.js
@@ -1,7 +1,16 @@
+const username = 'deinBenutzername';
+const password = 'deinPasswort';
+const kursIDs = [8372, 8621, 8615, 5544, 8532, 8528, 5493, 8620];
+const settings = {
+    autoLogin: true,
+    selectCourses: false,
+    autoBook: false
+}
+
 const url = "https://sawware.benno.webstitut.de"
 
 // ╭────────────────────────────────────────────────────────────────────────────────╮
-// │                                     Head                                       │
+// │                                     Meta                                       │
 // ╰────────────────────────────────────────────────────────────────────────────────╯
 
 const puppeteer = require('puppeteer');
@@ -16,17 +25,45 @@ function log(message) {
 // ╰────────────────────────────────────────────────────────────────────────────────╯
 
 (async () => {
-  const browser = await puppeteer.launch();
-  log("Browser gestartet");
-  const page = await browser.newPage();
-  log("Tab geöffnet");
-  await page.goto(`${url}/login`);
-  log(url, "geladen");
+    const browser = await puppeteer.launch();
+    log("Browser gestartet");
 
-  // Extrahiere den Titel der Seite
-  const title = await page.title();
-  console.log('Seiten-Titel:', title);
+    const page = await browser.newPage();
+    log("Tab geöffnet");
 
-  // Schließe den Browser
-  await browser.close();
+    await page.goto(`${url}/login`);
+    log(url, "geladen");
+
+    // ╭────────────────────────────────────────────────────────╮
+    // │                         Login                          │
+    // ╰────────────────────────────────────────────────────────╯
+
+    if (settings["autoLogin"]) {
+
+        document.getElementById('username').value = username;
+        document.getElementById('password').value = password;
+
+        // Auf den Submit-Button klicken
+        const submitButton = document.querySelector('button[type="submit"]');
+        if (submitButton) {
+            submitButton.click();
+        } else {
+            log('Submit-Button nicht gefunden!');
+    }};
+
+    // ╭────────────────────────────────────────────────────────╮
+    // │                        Booking                         │
+    // ╰────────────────────────────────────────────────────────╯
+
+    if (settings["courseBooking"]) {
+        page.goto(`${url}/coursebooking`)
+
+        kursIDs.forEach(kursID => {
+            Livewire.dispatch('addKurs', { kursID });
+        });
+
+        page.goto(`${url}/coursebooking/book`)
+    }
+
+    await browser.close();
 })();
