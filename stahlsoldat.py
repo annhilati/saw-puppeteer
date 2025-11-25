5584cd03bdcf963ea7c9e16a9fb8fb52a5c87c29
Annhilati <78727320+annhilati@users.noreply.github.com>
2025-11-25 12:03:53 +0100

    Sync

--- stahlsoldat.py
+++ stahlsoldat.py
@@ -1,8 +1,14 @@
 import asyncio
-from playwright.async_api import async_playwright
 import os, dotenv
 from fancyconsole import log
+from playwright.async_api import async_playwright, Page
 
+# 0. Anmelden
+# 1. Warten auf Beginn (detektieren?)
+#   ab 30 Sekunden vor: alle 5 Sekunden reloaden, Login-Page detektieren -> weiter
+# 
+# 
+# 
 
 # ────────────────────────────────────────────────────────────────────────────────
 # Meta / Config
@@ -12,6 +18,7 @@ dotenv.load_dotenv()
 username: str       = os.getenv("USER")
 password: str       = os.getenv("PASSWORD")
 kursIDs:  list[int] = [int(p) for p in os.getenv("KURSE").split(", ")]
+testlauf: bool      = os.getenv("TESTLAUF")
 
 settings = {
     "autoLogin": True,
@@ -29,94 +36,134 @@ url = "https://sawware.benno.webstitut.de"
 async def main():
 
     log("SCRIPT", "INFO", "Skript gestartet")
+    if testlauf:
+        log("SCRIPT", "WARN", "Dies ist ein Testlauf")
 
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch(
             headless=settings["headless"],
             args=["--start-maximized"]
         )
         log("BROWSER", "INFO", "Browser gestartet")
 
-        context = await browser.new_context(no_viewport=True)
-        page = await context.new_page()
+        ctx = await browser.new_context(no_viewport=True)
+        tab = await ctx.new_page()
         log("BROWSER", "INFO", "Neuer Tab geöffnet")
 
-        await page.goto(f"{url}/login")
-        log("BROWSER", "INFO", "Login-Seite geladen")
 
         # ──────────────────────────────────────────────
-        # Login
+        # Erster Login
         # ──────────────────────────────────────────────
 
-        if settings["autoLogin"]:
-            log("[SCRIPT]  [INFO] Login begonnen")
-            await page.fill("#username", username)
-            await page.fill("#password", password)
-            log("[LOGIN]   [INFO] Anmeldedaten eingetragen")
+        login(tab)
 
-            await page.wait_for_selector('button[type="submit"]', state="visible")
-            log("[LOGIN]   [INFO] Anmelde-Button sichtbar")
+        # Seitenzustand testen
+        await tab.goto(url + "/coursebooking")
+        log("BOOK", "INFO", "Warte eine Sekunde um eventuelle Weiterleitung abzuwarten")
+        try:
+            await tab.wait_for_url("**/dashboard", timeout=1000)
+            await tab.wait_for_load_state("load")
+        except:
+            pass
 
-            attempt = 1
-            maxAttempts = 60
+        if tab.url.endswith("/dashboard"):
+            log("BOOK", "WARN", "Nur Kursinformationen verfügbar")
+            await tab.goto(url + "/courseinformations")
+            log("BOOK", "SUCCESS", "Kursinformations-Seite geladen")
+        else:
+            log("BOOK", "SUCCESS", "Kursbuchungs-Seite geladen")
 
-            while attempt < maxAttempts:
+        # ──────────────────────────────────────────────
+        # Abwarten
+        # ──────────────────────────────────────────────
+        s = 5
+        if not testlauf:
+            log("SKRIPT", "INFO", f"Warten, bis Sessions beendet werden (Refresh alle {s} Sekunden)")
+            while not tab.url.endswith("login"):
                 try:
-                    await page.click('button[type="submit"]')
-                    log("[LOGIN]   [INFO] Anmeldedaten abgesendet (warte maximal 10 Sekunden auf Weiterleitung)")
+                    await tab.wait_for_url("**/login", timeout=s*1000)
+                except:
+                    await tab.reload()
 
-                    await page.wait_for_selector('button[wire\\:click="logout"]', timeout=10000)
-                    log(f"[LOGIN]   [SUCCESS] Angemeldet und Seite geladen: {page.url}")
-                    break
-
-                except Exception:
-                    log(f"[LOGIN]   [RETRY] Versuch {attempt} von {maxAttempts} gescheitert.")
-                    if attempt >= maxAttempts:
-                        log("[LOGIN]   [FATAL] Seite wurde nach maximalen Versuchen nicht weitergeleitet")
-                        return
-                    attempt += 1
+            log("SKRIPT", "INFO", f"Sessions wurden beendet")
+            login(tab)
 
         # ──────────────────────────────────────────────
         # Course Booking
         # ──────────────────────────────────────────────
 
-        if settings["selectCourses"]:
-            await page.goto(url + "/coursebooking")
-            log("[BOOK]    [INFO] Warte eine Sekunde um Weiterleitung abzuwarten")
-            await page.wait_for_timeout(1000)
+        await tab.goto(url + "/coursebooking")
+        log("BOOK", "INFO", "Warte eine Sekunde um Weiterleitung abzuwarten")
+        await tab.wait_for_timeout(1000)
 
-            if page.url.endswith("/dashboard"):
-                log("[BOOK]    [WARN] Nur Kursinformationen verfügbar")
-                await page.goto(url + "/courseinformations")
-                log("[BOOK]    [SUCCESS] Kursinformations-Seite geladen")
-            else:
-                log("[BOOK]    [SUCCESS] Kursbuchungs-Seite geladen")
+        if tab.url.endswith("/dashboard"):
+            log("BOOK", "WARN", "Nur Kursinformationen verfügbar")
+            await tab.goto(url + "/courseinformations")
+            log("BOOK", "SUCCESS", "Kursinformations-Seite geladen")
+        else:
+            log("BOOK", "SUCCESS", "Kursbuchungs-Seite geladen")
 
-            # Livewire dispatch ausführen
-            await page.evaluate(
-                """(kursIDs) => {
-                    kursIDs.forEach(kursID => {
-                        Livewire.dispatch('addKurs', { kursID });
-                    });
-                }""",
-                kursIDs
-            )
-            log("[BOOK]    [INFO] Alle Kurse versucht hinzuzufügen (Feedback erwarteter Maßen unbekannt)")
+        await tab.evaluate(
+            """(kursIDs) => {
+                kursIDs.forEach(kursID => {
+                    Livewire.dispatch('addKurs', { kursID });
+                });
+            }""",
+            kursIDs
+        )
+        log("BOOK", "INFO", "Alle Kurse versucht hinzuzufügen (Feedback erwarteter Maßen unbekannt)")
 
-        if settings["autoBook"] or settings["headless"]:
-            await page.goto(f"{url}/coursebooking/book")
-            log(f"[BOOK]    [INFO] Seite geladen: {page.url}")
+        await tab.goto(f"{url}/coursebooking/book")
+        log(f"BOOK", "INFO", f"Seite geladen: {tab.url}")
 
         # ──────────────────────────────────────────────
         # Ende
         # ──────────────────────────────────────────────
 
-        log("[SCRIPT]  [SUCCESS] Skript ordnungsgemäß ausgeführt")
+        log("SCRIPT", "SUCCESS", "Skript ordnungsgemäß ausgeführt")
         if settings["headless"]:
             await browser.close()
         else:
-            log("[SCRIPT]  [INFO] Schließe das Browserfenster, um das Skript zu beenden")
+            log("SCRIPT", "INFO", "Schließe das Browserfenster, um das Skript zu beenden")
 
+async def login(p: Page):
+    log("SKRIPT", "INFO", "Loginsequenz begonnen")
+
+    await p.goto(url + "/login")
+    try:
+        p.wait_for_url("**/dashboard", timeout=1000)
+        log("LOGIN", "SUCCESS", "Bereits angemeldet")
+        return
+    except:
+        pass
+
+    log("BROWSER", "INFO", "Login-Seite geladen")
+
+    await p.fill("#username", username)
+    await p.fill("#password", password)
+    log("LOGIN", "INFO", "Anmeldedaten eingetragen")
+
+    await p.wait_for_selector('button[type="submit"]', state="visible")
+    log("LOGIN", "INFO", "Anmelde-Button sichtbar")
+
+    attempt = 1
+    maxAttempts = 60
+
+    while attempt < maxAttempts:
+        try:
+            await p.click('button[type="submit"]')
+            log("LOGIN", "INFO", "Anmeldedaten abgesendet (warte maximal 10 Sekunden auf Weiterleitung)")
+
+            await p.wait_for_selector('button[wire\\:click="logout"]', timeout=10000)
+            log(f"LOGIN", "SUCCESS", f"Angemeldet und Seite geladen: {p.url}")
+            break
+
+        except Exception:
+            log(f"LOGIN", "FAIL", f"Versuch {attempt} von {maxAttempts} gescheitert.")
+            if attempt >= maxAttempts:
+                log("LOGIN", "FATAL", "Seite wurde nach maximalen Versuchen nicht weitergeleitet")
+                return
+            attempt += 1
 
 if __name__ == "__main__":
     asyncio.run(main())
